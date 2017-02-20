5ea603dd502712a341f143b3dc6d5b47e134b331
Zhixian Ma <zxma_sjtu@qq.com>
2017-02-20 21:05:28 +0800

    Modified and add lines for paramerters saving.

--- cnn/train_cnn.py
+++ cnn/train_cnn.py
@@ -16,10 +16,12 @@ def main():
     parser = argparse.ArgumentParser(description="Load data, and train the network.")
     # Parameters
     parser.add_argument("inpath", help="path of the samples")
+    parser.add_argument("outpath", help="path to save result.")
     parser.add_argument("numepoch", help="Number of epochs")
     args = parser.parse_args()
 
     inpath = args.inpath
+    outpath = args.outpath
     numepoch = int(args.numepoch)
 
     if not os.path.exists(inpath):
@@ -29,23 +31,32 @@ def main():
     # Load data
     x_train,y_train,x_val,y_val,x_test,y_test,boxsize = utils.load_data(inpath=inpath)
     # build the network
+    '''
     input_var = utils.T.tensor4('inputs')
     numclass=3
     kernel_size=[2,3,4]
     kernel_num=[12,12,12]
     pool_flag=[False,False,False]
-    '''
+
     network = utils.cnn_build(boxsize=boxsize, num_class=numclass,
                               kernel_size=kernel_size, kernel_num=kernel_num,
                               pool_flag=pool_flag, input_var=input_var)
     '''
     # train
-    network = utils.cnn_train(inputs_train=x_train, targets_train=y_train,
+    network, test_fn, est_fn = utils.cnn_train(inputs_train=x_train, targets_train=y_train,
                               inputs_val=x_val, targets_val=y_val,
                               network = None,
                               batchsize=100, num_epochs=numepoch)
     # test
-    # utils.cnn_test(inputs=x_test, targets=y_test, network=network, batchsize=100)
+    utils.cnn_test(inputs=x_test, targets=y_test, network=network, test_fn = test_fn, batchsize=100)
+    # save model
+    savepath = os.path.join(outpath,'model.pkl')
+    savedict = {'network':network, 'est_fn':est_fn}
+    utils.cnn_save(savepath=savepath, savedict=savedict)
+    # save params
+    parampath = os.path.join(outpath, 'params.mat')
+    utils.get_map(network, parampath)
+
 
 if __name__ == "__main__":
     main()
