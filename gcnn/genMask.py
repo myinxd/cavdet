afa09f38263a28a0204c20f2085cc1f44189b553
Zhixian Ma <zxma_sjtu@qq.com>
2017-02-27 14:48:47 +0800

    A script to generate the mask image.

---
+++ gcnn/genMask.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2017 Zhixian MA <zxma_sjtu@qq.com>
+
+"""
+Get mask images, pixels are marked with three values, 0,1 ,and 2.
+
+|:-------:| ------- |
+| Regions |  Mark   |
+|  BKG    |   0     |
+|  CAV    |   1     |
+|  CNT    |   2     |
+
+"""
+
+import os
+import argparse
+
+import cnn_utils as utils
+
+'''
+def main():
+    """The main function"""
+
+    # Init
+    parser = argparse.ArgumentParser(description="Generate marked images.")
+    # Parameters
+    parser.add_argument("obspath", help="path of the observation")
+    args = parser.parse_args()
+
+    obspath = args.obspath
+
+    # Judge existance of the path
+    if not os.path.exists(obspath):
+        print("The observation %s does not exist." % obspath)
+        return
+
+    cntpath = os.path.join(obspath, 'cnt.reg')
+    cavpath = os.path.join(obspath, 'cav_man.reg')
+    markpath = os.path.join(obspath, 'img_mark.png')
+    imgpath = os.path.join(obspath, 'img_beta.fits')
+    # get cav_mat
+    cav_mat = utils.get_reg(cntpath, cavpath)
+    # get mark_image
+    utils.get_mark_multi(imgpath, cav_mat, savepath=markpath)
+
+'''
+
+
+def main():
+    """The main function"""
+
+    # Init
+    parser = argparse.ArgumentParser(description="Generate marked images.")
+    # Parameters
+    parser.add_argument("inpath", help="path of the folder saving samples")
+    args = parser.parse_args()
+
+    inpath = args.inpath
+
+    if not os.path.exists(inpath):
+        print("The inpath does not exist.")
+        return
+
+    samples = os.listdir(inpath)
+    for obs in samples:
+        obspath = os.path.join(inpath, obs)
+        # Judge existance of the path
+        if not os.path.exists(obspath):
+            print("The observation %s does not exist." % obspath)
+        else:
+            print("Processing on sample %s ..." % obs)
+            cntpath = os.path.join(obspath, 'cnt.reg')
+            cavpath = os.path.join(obspath, 'cav_man.reg')
+            markpath = os.path.join(obspath, 'img_mark.png')
+            imgpath = os.path.join(obspath, 'beta.fits')
+            # get cav_mat
+            cav_mat = utils.get_reg(cntpath, cavpath)
+            # get mark_image
+            utils.get_mark_multi(imgpath, cav_mat, savepath=markpath)
+
+if __name__ == "__main__":
+    main()
